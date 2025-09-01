b39a74c1360fc6baf958526b629f312808724e09
Jonathas Piske <jonathaspiske@gmail.com>
2025-09-01 14:09:26 -0300

    Revert "Adiciona comentário"
    This reverts commit 2c88d527eda8a3e4b5743e8c164058b403e4160e.

--- hello.c
+++ hello.c
@@ -3,16 +3,6 @@
   int main(){
     int ret;
     ret = soma(2, 3);
-    JORDE
-    JORGE
-    JORGE JOGE
-    JONE
-    JONI
-    SE CUIDE JONI
-    SI CUIDE JONI
-    SI CUIDI JONI
-    SI CIIDI JONI
-    SI CIIDI JINI
     printf("O resultado é: %d", ret);
     return 0;
   }
